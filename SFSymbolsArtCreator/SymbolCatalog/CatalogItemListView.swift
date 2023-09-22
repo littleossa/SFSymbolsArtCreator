a69b5d991841cc783aca19fa52c490cfb09e13c9
littleossa <littleossa@gmail.com>
2023-09-22 22:56:56 +0900

    Improve: SymbolCatalogView and rename

---
+++ SFSymbolsArtCreator/SymbolCatalog/CatalogItemListView.swift
@@ -0,0 +1,156 @@
+//
+//  CatalogItemListView.swift
+//  SFSymbolsArtCreator
+//
+//
+
+import ComposableArchitecture
+import SFUserFriendlySymbols
+import SwiftUI
+
+struct CatalogItemListFeature: Reducer {
+    
+    struct State: Equatable {
+        @BindingState var backgroundCanvasColor: Color
+        var catalogItems: IdentifiedArrayOf<CatalogItemFeature.State>
+        @BindingState var category: SFSymbols.Category
+        @BindingState var searchText = ""
+        
+        init(backgroundCanvasColor: Color = .white,
+             category: SFSymbols.Category = .all,
+             searchText: String = "") {
+            self.backgroundCanvasColor = backgroundCanvasColor
+            self.category = category
+            
+            let allItems = category.symbols.compactMap { symbol in
+                CatalogItemFeature.State(
+                    canvasColor: backgroundCanvasColor,
+                    canvasLength: 72,
+                    id: UUID(),
+                    symbol: symbol
+                )
+            }
+            self.catalogItems = IdentifiedArray(uniqueElements: allItems)
+            self.searchText = searchText
+        }
+        
+        var filterCatalogItems: IdentifiedArrayOf<CatalogItemFeature.State> {
+            
+            let filteredSymbols: [SFSymbols]
+            if searchText.isEmpty {
+                filteredSymbols = category.symbols
+            } else {
+                let lowerCasedText = searchText.lowercased()
+                filteredSymbols = category.symbols.filter { $0.rawValue.contains(lowerCasedText) }
+            }
+            
+            if filteredSymbols.isEmpty {
+                return []
+            } else {
+                let filteredItems = filteredSymbols.compactMap { symbol in
+                    CatalogItemFeature.State(
+                        canvasColor: backgroundCanvasColor,
+                        canvasLength: 72,
+                        id: UUID(),
+                        symbol: symbol
+                    )
+                }
+                return IdentifiedArray(uniqueElements: filteredItems)
+            }
+        }
+    }
+    
+    enum Action: BindableAction, Equatable {
+        case binding(BindingAction<State>)
+        case catalogItem(id: CatalogItemFeature.State.ID, action: CatalogItemFeature.Action)
+        case delegate(Delegate)
+        case selectCategory(SFSymbols.Category)
+        
+        enum Delegate: Equatable {
+            case selectSymbol(SFSymbols)
+        }
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+                
+            case .binding:
+                return .none
+            case let .catalogItem(id: _, action: .delegate(.selectSymbol(symbol))):
+                return .run { send in
+                    await send(.delegate(.selectSymbol(symbol)))
+                }
+            case .catalogItem:
+                return .none
+            case let .delegate(.selectSymbol(symbol)):
+                // TODO: symbolを選択された時の処理
+                print("select", symbol)
+                return .none
+            case .delegate:
+                return .none
+            case let .selectCategory(category):
+                state.category = category
+                return .none
+            }
+        }
+        .forEach(\.catalogItems, action: /Action.catalogItem(id:action:)) {
+            CatalogItemFeature()
+        }
+    }
+}
+
+struct CatalogItemListView: View {
+    
+    private let columns: [GridItem] = .init(repeating: .init(spacing: 8), count: 3)
+    
+    let store: StoreOf<CatalogItemListFeature>
+    
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            
+            VStack(spacing: 16) {
+                
+                SearchBar(text: viewStore.$searchText)
+                
+                ScrollView {
+                    
+                    LazyVGrid(columns: columns) {
+                        
+                        ForEachStore(
+                            self.store.scope(state: \.filterCatalogItems,
+                                             action: CatalogItemListFeature.Action.catalogItem(id:action:))
+                        ) {
+                            CatalogItemView(store: $0)
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .background(Color(uiColor: .darkGray))
+        }
+        .frame(width: 284)
+    }
+}
+
+#Preview {
+    
+    HStack {
+        
+        ZStack {
+            Color(uiColor: .darkGray)
+                .frame(width: 284)
+            
+            CatalogItemListView(store: .init(
+                initialState: CatalogItemListFeature.State(
+                    backgroundCanvasColor: .white
+                )
+            ) {
+                CatalogItemListFeature()
+                    ._printChanges()
+            })
+        }
+        Spacer()
+    }
+}
