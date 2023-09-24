5bb5b08c74f47b91b61e69766535d263a8708d40
littleossa <littleossa@gmail.com>
2023-09-24 23:55:12 +0900

    Add: CatalogSettingsView

---
+++ SFSymbolsArtCreator/SymbolCatalog/CatalogSettingsView.swift
@@ -0,0 +1,57 @@
+//
+//  CatalogSettingView.swift
+//  SFSymbolsArtCreator
+//
+//
+
+import ComposableArchitecture
+import SFUserFriendlySymbols
+import SwiftUI
+
+struct CatalogSettingsFeature: Reducer {
+    struct State: Equatable {
+        @BindingState var backgroundColor: Color
+        @BindingState var category: SFSymbols.Category
+        @BindingState var symbolWeight: Font.Weight
+    }
+    
+    enum Action: BindableAction, Equatable {        
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+    }
+}
+
+struct CatalogSettingsView: View {
+    
+    let store: StoreOf<CatalogSettingsFeature>
+    
+    var body: some View {
+        
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                CategorySettingView(selectedCategory: viewStore.$category)
+                SymbolWeightSettingView(weight: viewStore.$symbolWeight)
+                CatalogBackgroundColorSettingView(color: viewStore.$backgroundColor)
+            }
+        }
+    }
+}
+
+#Preview {
+    Color.heavyDarkGray
+        .overlay {
+            CatalogSettingsView(store: .init(
+                initialState: CatalogSettingsFeature.State(
+                    backgroundColor: .white,
+                    category: .all,
+                    symbolWeight: .regular
+                ), reducer: {
+                    CatalogSettingsFeature()
+                })
+            )
+            .frame(width: 284, height: 500)
+        }
+}
