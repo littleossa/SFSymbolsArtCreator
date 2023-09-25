08d81c6d007c3221dd13790c64a15a394eba2db7
littleossa <littleossa@gmail.com>
2023-09-26 08:53:34 +0900

    Add: CatalogItemListFeatureTests

---
+++ SFSymbolsArtCreatorTests/CatalogItemListFeatureTests.swift
@@ -0,0 +1,47 @@
+//
+//  CatalogItemListFeatureTests.swift
+//  SFSymbolsArtCreatorTests
+//
+//
+
+import ComposableArchitecture
+import SFUserFriendlySymbols
+import XCTest
+@testable import SFSymbolsArtCreator
+
+@MainActor
+final class CatalogItemListFeatureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func test_search() async {
+        let store = TestStore(
+            initialState: CatalogItemListFeature.State(
+                fontWeight: .regular,
+                primaryColor: .black,
+                secondaryColor: .black,
+                tertiaryColor: .clear,
+                renderingType: .monochrome,
+                backgroundColor: .white,
+                category: .all)
+        ) {
+            CatalogItemListFeature()
+        }
+        
+        await store.send(.binding(.set(\.$searchText, "xmark"))) {
+            $0.searchText = "xmark"
+        }
+        
+        XCTAssertTrue(store.state.catalogItems.contains(SFSymbols.xmark))
+        XCTAssertTrue(store.state.catalogItems.contains(SFSymbols.xmarkCircle))
+        XCTAssertFalse(store.state.catalogItems.contains(SFSymbols._00CircleFill))
+        
+        await store.send(.binding(.set(\.$searchText, ""))) {
+            $0.searchText = ""
+        }
+    }
+}
