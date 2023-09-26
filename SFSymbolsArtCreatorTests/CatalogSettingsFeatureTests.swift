d2653dfe220d908b297835eaf6616fd7823aedf7
littleossa <littleossa@gmail.com>
2023-09-26 09:02:14 +0900

    Add: CatalogSettingsFeatureTests

---
+++ SFSymbolsArtCreatorTests/CatalogSettingsFeatureTests.swift
@@ -0,0 +1,38 @@
+//
+//  CatalogSettingsFeatureTests.swift
+//  SFSymbolsArtCreatorTests
+//
+//
+
+import ComposableArchitecture
+import SwiftUI
+import XCTest
+@testable import SFSymbolsArtCreator
+
+@MainActor
+final class CatalogSettingsFeatureTests: XCTestCase {
+    
+    func test_catalogSettings() async {
+        let store = TestStore(
+            initialState: CatalogSettingsFeature.State(
+                backgroundColor: .white,
+                category: .all,
+                symbolWeight: .regular
+            )
+        ) {
+            CatalogSettingsFeature()
+        }
+        
+        await store.send(.binding(.set(\.$backgroundColor, .black))) {
+            $0.backgroundColor = .black
+        }
+        
+        await store.send(.binding(.set(\.$category, .devices))) {
+            $0.category = .devices
+        }
+        
+        await store.send(.binding(.set(\.$symbolWeight, .bold))) {
+            $0.symbolWeight = .bold
+        }
+    }
+}
