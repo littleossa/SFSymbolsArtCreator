861fb539a654bd41b3629b3c8aff3281ff1ffdf2
littleossa <littleossa@gmail.com>
2023-09-27 08:38:21 +0900

    Test: ColorToolFeature

---
+++ SFSymbolsArtCreatorTests/ColorToolBarTests/ColorToolFeatureTests.swift
@@ -0,0 +1,66 @@
+//
+//  ColorToolFeatureTests.swift
+//  SFSymbolsArtCreatorTests
+//
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import SFSymbolsArtCreator
+
+@MainActor
+final class ColorToolFeatureTests: XCTestCase {
+
+    func test_colorButtonTapped() async {
+        let store = TestStore(initialState: ColorToolFeature.State(renderingType: .monochrome)) {
+            ColorToolFeature()
+        }
+        
+        await store.send(.primaryColorButtonTapped) {
+            $0.colorPicker = ColorPickerFeature.State(colorType: .primary, selectedColor: $0.primaryColor)
+        }
+        
+        await store.send(.secondaryColorButtonTapped) {
+            $0.colorPicker = ColorPickerFeature.State(colorType: .secondary, selectedColor: $0.secondaryColor)
+        }
+        
+        await store.send(.tertiaryColorButtonTapped) {
+            $0.colorPicker = ColorPickerFeature.State(colorType: .tertiary, selectedColor: $0.tertiaryColor)
+        }
+        
+        await store.send(.canvasColorButtonTapped) {
+            $0.colorPicker = ColorPickerFeature.State(colorType: .canvas, selectedColor: $0.canvasColor)
+        }
+    }
+    
+    func test_isOnlyPrimaryColorEnabled() async {
+        
+        XCTContext.runActivity(named: "Monochrome is only primary color enabled") { _ in
+            let store = TestStore(initialState: ColorToolFeature.State(renderingType: .monochrome)) {
+                ColorToolFeature()
+            }
+            XCTAssertTrue(store.state.isOnlyPrimaryColorEnabled)
+        }
+        
+        XCTContext.runActivity(named: "MultiColor is not only primary color enabled") { _ in
+            let store = TestStore(initialState: ColorToolFeature.State(renderingType: .multiColor)) {
+                ColorToolFeature()
+            }
+            XCTAssertFalse(store.state.isOnlyPrimaryColorEnabled)
+        }
+        
+        XCTContext.runActivity(named: "Hierarchical is only primary color enabled") { _ in
+            let store = TestStore(initialState: ColorToolFeature.State(renderingType: .hierarchical)) {
+                ColorToolFeature()
+            }
+            XCTAssertTrue(store.state.isOnlyPrimaryColorEnabled)
+        }
+        
+        XCTContext.runActivity(named: "Palette is only primary color enabled") { _ in
+            let store = TestStore(initialState: ColorToolFeature.State(renderingType: .palette)) {
+                ColorToolFeature()
+            }
+            XCTAssertTrue(store.state.isOnlyPrimaryColorEnabled)
+        }
+    }
+}
