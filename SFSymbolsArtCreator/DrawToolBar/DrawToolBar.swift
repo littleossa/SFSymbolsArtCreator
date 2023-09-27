cc7a44c138268e3a221d47472338ea021963d333
littleossa <littleossa@gmail.com>
2023-09-27 13:06:42 +0900

    Move: RenderingMenuButton from DrawToolBar file

--- SFSymbolsArtCreator/DrawToolBar/DrawToolBar.swift
+++ SFSymbolsArtCreator/DrawToolBar/DrawToolBar.swift
@@ -108,39 +108,3 @@ struct DrawToolBar: View {
             })
         }
 }
-
-struct RenderingMenuButton: View {
-    
-    let type: RenderingType
-    let selectAction: (_ renderingType: RenderingType) -> Void
-    
-    var body: some View {
-        Menu {
-            ForEach(RenderingType.allCases) { type in
-                
-                Button(action: {
-                    selectAction(type)
-                }, label: {
-                    Text(type.displayLabel)
-                })
-            }
-            
-        } label: {
-            RoundedRectangle(cornerRadius: 8)
-                .fill(.paleGray)
-                .overlay {
-                    Text(self.type.displayLabel)
-                    .font(.title2)
-                }
-        }
-    }
-}
-
-#Preview {
-    Color.black
-        .overlay {
-            RenderingMenuButton(type: .monochrome) { _ in
-            }
-            .frame(width: 200, height: 48)
-        }
-}
