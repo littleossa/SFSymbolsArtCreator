a4e937604b4e82bb93ecc31eb88a30127f8a613d
littleossa <littleossa@gmail.com>
2023-10-02 08:39:59 +0900

    Add: CatalogItem

---
+++ SFSymbolsArtCreator/Models/CatalogItem.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogItem.swift
+//  SFSymbolsArtCreator
+//
+//
+
+import SwiftUI
+
+struct CatalogItem: Equatable {
+    let symbolName: String
+    let renderingType: RenderingType
+    let primaryColor: Color
+    let secondaryColor: Color
+    let tertiaryColor: Color
+    let fontWeight: Font.Weight
+}
+
