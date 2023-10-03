76f5a1eecf118b53e4d0c26534b0b87f5669aedf
littleossa <littleossa@gmail.com>
2023-10-03 10:03:30 +0900

    Refactor: RotationEffectAxis

--- SFSymbolsArtCreator/Models/FlipType.swift
+++ SFSymbolsArtCreator/Models/FlipType.swift
@@ -28,18 +28,23 @@ enum FlipType {
     case none
     case vertical
     
-    var rotationEffectAxis: (x: CGFloat, y: CGFloat, z: CGFloat) {
+    var rotationEffectAxis: RotationEffectAxis {
         
         switch self {
         case .horizontal:
-            return (x: 0, y: 1, z: 0)
+            return .init(x: 0, y: 1, z: 0)
         case .horizontalAndVertical:
-            return (x: 0, y: 0, z: 1)
+            return .init(x: 0, y: 0, z: 1)
         case .none:
-            return (x: 0, y: 0, z: 0)
+            return .init(x: 0, y: 0, z: 0)
         case .vertical:
-            return (x: 1, y: 0, z: 0)
+            return .init(x: 1, y: 0, z: 0)
         }
     }
 }
 
+struct RotationEffectAxis: Equatable {
+    let x: CGFloat
+    let y: CGFloat
+    let z: CGFloat
+}
