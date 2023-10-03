cbba3d45ac6134d2e00b871714fee5bf664da63e
littleossa <littleossa@gmail.com>
2023-10-03 10:07:48 +0900

    Test: FlipType

---
+++ SFSymbolsArtCreatorTests/ModelsTests/FlipTypeTests.swift
@@ -0,0 +1,41 @@
+//
+//  FlipTypeTests.swift
+//  SFSymbolsArtCreatorTests
+//
+//
+
+import XCTest
+@testable import SFSymbolsArtCreator
+
+final class FlipTypeTests: XCTestCase {
+
+    func test_init() {
+        
+        var value = FlipType(isFlippedHorizontal: true, isFlippedVertical: false)
+        XCTAssertEqual(value, .horizontal)
+        
+        value = FlipType(isFlippedHorizontal: true, isFlippedVertical: true)
+        XCTAssertEqual(value, .horizontalAndVertical)
+
+        value = FlipType(isFlippedHorizontal: false, isFlippedVertical: true)
+        XCTAssertEqual(value, .vertical)
+        
+        value = FlipType(isFlippedHorizontal: false, isFlippedVertical: false)
+        XCTAssertEqual(value, .none)
+    }
+    
+    func test_rotationEffectAxis() {
+        
+        XCTAssertEqual(RotationEffectAxis(x: 0, y: 1, z: 0),
+                       FlipType.horizontal.rotationEffectAxis)
+        
+        XCTAssertEqual(RotationEffectAxis(x: 0, y: 0, z: 1),
+                       FlipType.horizontalAndVertical.rotationEffectAxis)
+        
+        XCTAssertEqual(RotationEffectAxis(x: 1, y: 0, z: 0),
+                       FlipType.vertical.rotationEffectAxis)
+        
+        XCTAssertEqual(RotationEffectAxis(x: 0, y: 0, z: 0),
+                       FlipType.none.rotationEffectAxis)
+    }
+}
